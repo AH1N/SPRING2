5b3994c60ed7c88fc0f978c4010458edd6653d8c
AH1N <32333267+AH1N@users.noreply.github.com>
2024-05-08 21:42:41 +0300

    Delete HW7 directory

--- HW7/user-app/src/main/java/hw/hw6/userapp/entity/Userr.java
+++
@@ -1,9 +0,0 @@
-package hw.hw6.userapp.entity;
-
-/**
- * The type Userr.
- * класс для DTO вместо простого создания класса
- */
-public record Userr(Integer id, String username, String userEmail ) {
-
-}
